41f0c55b81ffa9cf09a0e622b9d814f799e0c19b
Eun Joo <eunjoolee787@gmail.com>
2014-10-25 11:05:08 -1000

    finish test 3

--- test/test-generator.js
+++ test/test-generator.js
@@ -19,4 +19,15 @@ describe("MultiDimensionalArray.generate", function(){
     array.length.should.equal(4);
   
   });
+
+  it("should return an array(length 5, 4)", function(){
+    var array = MDA.generate(5,4);
+    array.length.should.equal(5);
+
+    array.forEach(function(element){
+      element.length.should.equal(4);
+    });
+  });
+
+
 });
