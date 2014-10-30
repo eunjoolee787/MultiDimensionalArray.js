148149f274d934178c49a27637e8c43431c1f4b9
Eun Joo <eunjoolee787@gmail.com>
2014-10-29 19:15:56 -1000

    generate passing & count passes 1 test

---
+++ MultiDimensionalArray.js
@@ -0,0 +1,38 @@
+module.exports = {
+
+  generate:function(arrayLength1, arrayLength2){
+  
+    var arr = [];
+
+    for(var i = 0; i < arrayLength1; i++){
+      arr.push([]);
+    }
+    arr.forEach(function (element) {
+     for(var i = 0; i < arrayLength2; i++){
+      element.push(Math.floor(Math.random()));
+     } 
+    });
+    return arr;
+  },
+
+
+  count:function(array1, test){
+    
+    var myCount = 0;
+    for (var i = 0; i < array1.length; i++){
+      if (array1[i] == test){
+        myCount++;
+      }
+    }
+   return myCount;
+  }
+};
+
+
+
+
+
+
+
+
+
