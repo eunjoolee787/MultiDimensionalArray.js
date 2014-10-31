17ff5679ac986da72e3fae20d2918814ffd34d7f
Eun Joo <eunjoolee787@gmail.com>
2014-10-30 21:01:01 -1000

    finish round 1 in count

--- MultiDimensionalArray.js
+++ MultiDimensionalArray.js
@@ -1,15 +1,13 @@
 module.exports = {
 
   generate:function(arrayLength1, arrayLength2){
-  
     var arr = [];
-
     for(var i = 0; i < arrayLength1; i++){
       arr.push([]);
     }
     arr.forEach(function (element) {
      for(var i = 0; i < arrayLength2; i++){
-      element.push(Math.floor(Math.random()));
+      element.push(Math.floor(Math.random()*2));
      } 
     });
     return arr;
@@ -20,7 +18,9 @@ module.exports = {
     
     var myCount = 0;
     for (var i = 0; i < array1.length; i++){
-      if (array1[i] == test){
+      if (array1[i] instanceof Array){
+        myCount+= this.count(array1[i]);
+      } else if (array1[i] == test){
         myCount++;
       }
     }
@@ -28,6 +28,9 @@ module.exports = {
   }
 };
 
+
+
+  
 
 
 
